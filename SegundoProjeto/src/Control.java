08026cd1b852d2b17cdbbb1e9034573a9b4dcddd
whklein <whklein@192.168.56.1>
2019-05-13 21:30:06 -0300

    Alteração de nomenclaturas.

--- SegundoProjeto/src/Control.java
+++ SegundoProjeto/src/Control.java
@@ -15,12 +15,12 @@ public class Control
         List<String> lines = Files.readAllLines(Paths.get("H:\\SecondProject\\src\\files\\game-reviews.csv"));
         lines = lines.subList(1, lines.size());
 
-        Map<String, List<Review>> reviewsYear = lines.stream()
+        Map<String, List<Review>> avaliacoes = lines.stream()
                                                      .map(Control::convertToReview)
                                                      .collect(groupingBy(Review::getReleaseYear, TreeMap::new, toList()));
 
-        baseControl(reviewsYear);
-        exibirAnoLancamentoMaiorNumeroJogosAcao(reviewsYear);
+        controleBase(avaliacoes);
+        exibirAnoLancamentoMaiorNumeroJogosAcao(avaliacoes);
     }
 
     private static Review convertToReview(String line) 
@@ -29,14 +29,14 @@ public class Control
         return new Review(values[0], values[6], values[2], values[4], Double.parseDouble(values[3]));
     }
 
-    private static void exibirAnoLancamentoMaiorNumeroJogosAcao(Map<String, List<Review>> reviewsYear) 
+    private static void exibirAnoLancamentoMaiorNumeroJogosAcao(Map<String, List<Review>> avaliacoes) 
     {
         long biggestActionGamesAmount = 0;
         String yearBiggestActionGamesAmount = null;
 
-        for (String year : reviewsYear.keySet()) 
+        for (String year : avaliacoes.keySet()) 
         {
-            long actionGamesAmount = reviewsYear.get(year).stream().filter(review -> review.getGenre().equals("Action")).map(Review::getGame).distinct().count();
+            long actionGamesAmount = avaliacoes.get(year).stream().filter(review -> review.getGenre().equals("Action")).map(Review::getGame).distinct().count();
 
             if (biggestActionGamesAmount < actionGamesAmount) 
             {
@@ -48,9 +48,9 @@ public class Control
         System.out.println("Ano em que foi lançado maior número de jogos do gênero ‘Action’?: " + yearBiggestActionGamesAmount);
     }
 
-    private static void baseControl(Map<String, List<Review>> reviewsYear) 
+    private static void controleBase(Map<String, List<Review>> avaliacoes) 
     {
-        reviewsYear.forEach((year, reviews) -> 
+        avaliacoes.forEach((year, reviews) -> 
         {
             double sumScore = 0;
             double mediocre = 0;
@@ -62,20 +62,20 @@ public class Control
             }
 
             double getMediaAritmetica = sumScore / reviews.size();
-            List<String> worstToBestReviews = sortReviewsFromWorstToBest(reviews);
+            List<String> piorParaMaior = ordenaDePiorParaMaior(reviews);
 
             System.out.println();
             System.out.println(String.format("Número de reviews no ano %s: %s", year, reviews.size()));
             System.out.println(String.format("Mediocre reviews: %.2f%%", (mediocre / reviews.size()) * 100));
             System.out.println(String.format("Média aritmética: %.2f", getMediaAritmetica));
             System.out.println(String.format("Desvio padrão populacional: %.2f", getDesvioPadrao(reviews, getMediaAritmetica)));
-            System.out.println(String.format("Melhor jogo: %s", worstToBestReviews.get(worstToBestReviews.size() - 1)));
-            System.out.println(String.format("Pior jogo: %s", worstToBestReviews.get(0)));
+            System.out.println(String.format("Melhor jogo: %s", piorParaMaior.get(piorParaMaior.size() - 1)));
+            System.out.println(String.format("Pior jogo: %s", piorParaMaior.get(0)));
             System.out.println("\n---------------------------------");
         });
     }
 
-    private static List<String> sortReviewsFromWorstToBest(List<Review> reviews) 
+    private static List<String> ordenaDePiorParaMaior(List<Review> reviews) 
     {
         return reviews.stream().collect(groupingBy(Review::getGame, summingDouble(Review::getScore))).entrySet().stream().sorted(Comparator.comparing(Map.Entry::getValue)).map(Map.Entry::getKey).collect(toList());
     }
